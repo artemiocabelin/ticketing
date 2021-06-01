d8c430f0658d695bdfaba5f56e25147b12d0cdeb
Artemio Cabelin <artemiocabelin@gmail.com>
2021-05-31 22:05:27 -0700

    fix ingress and app cookie on all services

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
 
     return axios.create({
-      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.some-random-test-app.xyz',
       headers: req.headers
     });
 
